6ebcace8ccda78d0deb720737b78271958357e8a
fuji <fujiyoshiyuki@gmail.com>
2021-03-01 21:52:51 +0900

    typescriptで実装

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 /**
- * 
+ * 変換処理を実行する.
  */
-function execute(){
-    const spaceNum = Number(document.getElementById("spaceNum").value);
-    const inputText = document.getElementById("inputTextarea").value;
-    const inputTexts = inputText.trim().split('\n');
-    const maxLength = getMaxLength(inputTexts);
-
-    for (let i = 0; i < inputTexts.length; i++){
-        let space = " ".repeat((maxLength-inputTexts[i].length)+spaceNum)
-        inputTexts[i] = inputTexts[i]+space;
+function execute() {
+    var spaceNumHtml = document.getElementById("spaceNum");
+    var spaceNum = Number(spaceNumHtml.value);
+    var inputTextHtml = document.getElementById("inputTextarea");
+    var inputText = inputTextHtml.value;
+    var inputTexts = inputText.split("\n");
+    var maxLength = getMaxLength(inputTexts);
+    for (var i = 0; i < inputTexts.length; i++) {
+        var space = " ".repeat((maxLength - inputTexts[i].length) + spaceNum);
+        inputTexts[i] = inputTexts[i] + space;
     }
-    document.getElementById("outputTextarea").value = inputTexts.join("\n");
+    var outputTextarea = document.getElementById("outputTextarea");
+    outputTextarea.value = inputTexts.join("\n");
 }
-
 /**
  * 文字列のリストから最大の長さを持つ要素の長さを返す.
- * @param {文字列のリスト} inputTexts 
+ * @param inputTexts 文字列のリスト
  */
-function getMaxLength(inputTexts){
-    let maxLength = 0;
-    for (let i = 0; i < inputTexts.length; i++){
-        if (inputTexts[i].length > maxLength){
+function getMaxLength(inputTexts) {
+    var maxLength = 0;
+    for (var i = 0; i < inputTexts.length; i++) {
+        if (inputTexts[i].length > maxLength) {
             maxLength = inputTexts[i].length;
         }
     }
